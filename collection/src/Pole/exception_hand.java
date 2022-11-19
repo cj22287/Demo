61390f23099deadd8d13673a5424cf6d160159c6
cj22287 <118585302+cj22287@users.noreply.github.com>
2022-11-19 09:52:52 +0530

    done
    hi

--- collection/src/Pole/exception_hand.java
+++ collection/src/Pole/exception_hand.java
@@ -5,11 +5,12 @@ public class exception_hand {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-int a[]=new int[3];
+int a[]=new int[4];
 a[0]=78;
 a[1]=90;
 a[2]=86;
 a[3]=19;
+a[4]=22287;			
 System.out.println(a[2]);
 		}
 catch(Exception e) {
